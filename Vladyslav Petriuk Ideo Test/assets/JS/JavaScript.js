d6e06d1509d972225c316677f17c60420c835c21
w60083 <49841198+w60083@users.noreply.github.com>
2020-10-23 13:16:39 +0200

    Test Ideo
    Vladyslav Petriuk , zadanie jest wykonane

---
+++ Vladyslav Petriuk Ideo Test/assets/JS/JavaScript.js
@@ -0,0 +1,11 @@
+
+        function openMenu(){
+            document.getElementById("sidebar").classList.toggle('active');
+            document.getElementById("rotate").classList.toggle('active');
+            document.getElementById("textMenu").classList.toggle('active');
+        };
+       
+        function search(){
+             document.getElementById("search_input").classList.toggle('active');
+        }
+   
